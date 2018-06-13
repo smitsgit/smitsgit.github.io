d08348d7f7cf4fd29826573c62a7342bc596fde4
Smital Desai <smital.desai@go-jek.com>
2018-06-13 19:17:29 +0530

    Test google analytics with script tag

--- _static/google_analytics.js
+++ _static/google_analytics.js
@@ -1,7 +1,10 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120801664-1"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-120801664-1');
+</script>
 
