3f5dac5aeeb936fd2293447d33d4f097cba95aa3
Smital Desai <smital.desai@go-jek.com>
2018-06-15 11:22:02 +0530

    Experiment with tracking

--- _static/google_analytics.js
+++ _static/google_analytics.js
@@ -1,10 +1,7 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-120801664-1"></script>
-<script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
 
   gtag('config', 'UA-120801664-1');
-</script>
 
